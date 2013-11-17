892baa58e897693d6170327cd926b7d7925a2fc7
jeremyBass <jeremybass@cableone.net>
2013-11-16 16:58:13 -0800

    adds html packing to admin area

--- app/code/local/Wsu/Storeutilities/Model/Observer.php
+++ app/code/local/Wsu/Storeutilities/Model/Observer.php
@@ -15,41 +15,50 @@ class Wsu_Storeutilities_Model_Observer{
 		//$this->getResponse()->appendBody($output);
 	}
 	
+	public function admin_simpleHtmlMinify($observer){
+		if(Mage::getStoreConfig('storeutilities_conf/html/minify_admin_html_output')){
+			$response = $observer->getResponse(); 
+			$html     = $response->getBody(); 
+			$html = $this->quicktmlMinify($html);
+			$response->setBody($html); 
+		}
+	}
     public function simpleHtmlMinify($observer) {
         if (Mage::getStoreConfig('storeutilities_conf/html/minify_html_output')) {
-
             // Fetches the current event
             $event = $observer->getEvent();
             $controller = $event->getControllerAction();
             $allHtml = $controller->getResponse()->getBody();
 
-			$re = '%# Collapse whitespace everywhere but in blacklisted elements.
-				(?>             # Match all whitespans other than single space.
-				  [^\S ]\s*     # Either one [\t\r\n\f\v] and zero or more ws,
-				| \s{2,}        # or two or more consecutive-any-whitespace.
-				) # Note: The remaining regex consumes no text at all...
-				(?=             # Ensure we are not in a blacklist tag.
-				  [^<]*+        # Either zero or more non-"<" {normal*}
-				  (?:           # Begin {(special normal*)*} construct
-					<           # or a < starting a non-blacklist tag.
-					(?!/?(?:textarea|pre|script)\b)
-					[^<]*+      # more non-"<" {normal*}
-				  )*+           # Finish "unrolling-the-loop"
-				  (?:           # Begin alternation group.
-					<           # Either a blacklist start tag.
-					(?>textarea|pre|script)\b
-				  | \z          # or end of file.
-				  )             # End alternation group.
-				)  # If we made it here, we are not in a blacklist tag.
-				%Six';
-			$content = preg_replace($re, " ", $allHtml);
-
-
-
+			$content = $this->quicktmlMinify($allHtml);
             $controller->getResponse()->setBody($content);
         }
     }
-
+	
+	//move this
+	public function quicktmlMinify($text) {
+		$re = '%# Collapse whitespace everywhere but in blacklisted elements.
+			(?>             # Match all whitespans other than single space.
+			  [^\S ]\s*     # Either one [\t\r\n\f\v] and zero or more ws,
+			| \s{2,}        # or two or more consecutive-any-whitespace.
+			) # Note: The remaining regex consumes no text at all...
+			(?=             # Ensure we are not in a blacklist tag.
+			  [^<]*+        # Either zero or more non-"<" {normal*}
+			  (?:           # Begin {(special normal*)*} construct
+				<           # or a < starting a non-blacklist tag.
+				(?!/?(?:textarea|pre|script)\b)
+				[^<]*+      # more non-"<" {normal*}
+			  )*+           # Finish "unrolling-the-loop"
+			  (?:           # Begin alternation group.
+				<           # Either a blacklist start tag.
+				(?>textarea|pre|script)\b
+			  | \z          # or end of file.
+			  )             # End alternation group.
+			)  # If we made it here, we are not in a blacklist tag.
+			%Six';
+		$text = preg_replace($re, " ", $text);
+		return $text;
+    }
 	
     public function injectStoreutilitiesButton($observer) {
         $block = $observer->getEvent()->getBlock();
