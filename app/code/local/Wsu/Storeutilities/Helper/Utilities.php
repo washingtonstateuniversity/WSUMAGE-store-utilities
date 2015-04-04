4b6b2c01fd109a298055c1f98ae20b15e23ecb0f
Jeremy Bass <jeremybass@cableone.net>
2015-04-04 03:14:02 -0700

    remove a function that is covered in the core

--- app/code/local/Wsu/Storeutilities/Helper/Utilities.php
+++ app/code/local/Wsu/Storeutilities/Helper/Utilities.php
@@ -1,12 +1,8 @@
 <?php
 
 class Wsu_Storeutilities_Helper_Utilities extends Mage_Core_Helper_Abstract {
-	
-	public function logInfo($String){
-			echo $String."<br/>";
-	}
-	
-	
+
+
 	public function getUniqueCode($length = ""){
 		$code = md5(uniqid(rand(), true));
 		if ($length != "") return substr($code, 0, $length);
